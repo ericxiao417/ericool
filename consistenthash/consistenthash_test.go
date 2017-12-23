48471edf3c0b52a00c37241ef716b3099baba80b
ericxiao417 <xiaozhen0417@126.com>
2017-12-23 13:45:00 +0000

    Add hash package

---
+++ consistenthash/consistenthash_test.go
@@ -0,0 +1,87 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"math"
+	"strconv"
+	"testing"
+
+	"github.com/ericxiao417/ericool/assert"
+	"github.com/ericxiao417/ericool/ericlog"
+)
+
+func TestConsistentHash(t *testing.T) {
+	ch := New(1024)
+	_, err := ch.Get("aaa")
+	assert.Equal(t, ErrIsEmpty, err)
+
+	ch.Add("127.0.0.1")
+	ch.Add("0.0.0.0", "8.8.8.8")
+	ch.Del("127.0.0.1", "8.8.8.8")
+	ch.Add("114.114.114.114", "255.255.255.255", "1.1.1.1", "2.2.2.2", "3.3.3.3")
+	exptectedNodes := []string{
+		"0.0.0.0",
+		"114.114.114.114",
+		"255.255.255.255",
+		"1.1.1.1",
+		"2.2.2.2",
+		"3.3.3.3",
+	}
+	actualNodes := ch.Nodes()
+	assert.Equal(t, len(exptectedNodes), len(actualNodes))
+	for _, en := range exptectedNodes {
+		_, ok := actualNodes[en]
+		assert.Equal(t, true, ok)
+	}
+
+	counts := make(map[string]int)
+	for i := 0; i < 16384; i++ {
+		node, err := ch.Get(strconv.Itoa(i))
+		assert.Equal(t, nil, err)
+		counts[node]++
+	}
+	ericlog.Debugf("%+v", counts)
+}
+
+func TestConsistentHash_Nodes(t *testing.T) {
+	nodesGolden := []string{
+		"0.0.0.0",
+		"114.114.114.114",
+		"255.255.255.255",
+		"1.1.1.1",
+		"2.2.2.2",
+		"3.3.3.3",
+	}
+	j := 1024 // 1
+	k := 1024 // 16384
+	for i := j; i <= k; i = i << 1 {
+		ericlog.Debugf("-----%d-----", i)
+		ch := New(i)
+		ch.Add(nodesGolden...)
+		nodes := ch.Nodes()
+		count := uint64(0)
+		for k, v := range nodes {
+			count += uint64(v)
+			ericlog.Debugf("%s: %+v", k, float32(v)/float32(math.MaxUint32+1))
+		}
+		assert.Equal(t, uint64(math.MaxUint32+1), count)
+	}
+}
+
+func TestCorner(t *testing.T) {
+	ch := New(1, func(option *Option) {
+		option.hfn = crc32.ChecksumIEEE
+	})
+
+	ch = New(1)
+	nodes := ch.Nodes()
+	assert.Equal(t, nil, nodes)
+
+	ch = New(1)
+	ch.Add("127.0.0.1")
+	nodes = ch.Nodes()
+	exptectedNodes := map[string]uint64{
+		"127.0.0.1": math.MaxUint32 + 1,
+	}
+	assert.Equal(t, exptectedNodes, nodes)
+}
